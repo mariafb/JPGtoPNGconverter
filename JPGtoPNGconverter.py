3726cef6def5db8ea1564fa20445a0298ce00b6a
mariafb <mariafb@users.noreply.github.com>
2021-02-21 20:25:11 +0000

    new commit

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -10,15 +10,11 @@ output_folder = sys.argv[2]
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
+# loop through pokedex
+# convert images to png
+# save to new folder
 for filename in os.listdir(image_folder):
     img = Image.open(f'{image_folder}{filename}')
     clean_name = os.path.splitext(filename)[0]
     img.save(f'{output_folder}{clean_name}.png', 'png')
     print('all done !')
-
-
-# loop through pokedex
-
-# convert images to png
-
-# save to new folder
